d869c9cbfa6a305c405fecfff9b4a619e906b666
quentin <clavier.quentin@gmail.com>
2022-09-22 12:27:26 +0200

    commit before merge

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,19 @@ use App\Http\Controllers\Api\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('item', App\Http\Controllers\ItemController::class);
+
+Route::resource('item', App\Http\Controllers\ItemController::class);
+
+Route::resource('order', OrderController::class)->middleware('auth:sanctum');
+
+Route::get('buy', [OrderController::class, 'validateOrder'])->middleware('auth:sanctum');
+
+Route::get('cart', [ItemController::class,'cart'])->middleware('auth:sanctum');
+
+Route::delete('cart/{item}', [ItemController::class,'deleteArticle'])->middleware('auth:sanctum');
+
+Route::post('cart/{item}', [ItemController::class,'ajoutArticle'])->middleware('auth:sanctum');
+
 Route::post('/register',[AuthController::class, 'registerUser']);
 Route::post('/login',[AuthController::class, 'loginuser']);
 Route::get('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
